5b979cf8a04444bc63ccb69ed4cb0a2f139e24f1
bohdanrom <karpovucc@ukr.net>
2021-04-23 03:12:36 +0300

    Add game page and page with list of games

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import json
 import base64
 import jinja2
 from flask import Flask, render_template, request
@@ -17,19 +16,10 @@ def main_page():
 def display_game(game_id: int):
     game_details = models.db.Games.query.filter_by(game_id=game_id).first()
     game_photo = models.db.GameImages.query.filter_by(game_id=game_id).first()
-    # game_genres = models.db.Games.query.join(models.db.game_and_game_types).join(models.db.GameTypes).filter((models.db.game_and_game_types.c.game_id == models.db.Games.game_id) & (models.db.game_and_game_types.c.game_type_id == models.db.GameTypes.game_type_id)).all()
-    game_genres = models.db.db.session.execute("SELECT gt.game_type_name, gatp.game_id FROM Games g "
-                                            "INNER JOIN Games_and_Game_Types gatp on g.game_id = gatp.game_id "
-                                            "INNER JOIN Game_Types gt on gatp.game_type_id = gt.game_type_id "
-                                            "WHERE gatp.game_id = :game_id", {'game_id': game_id})
-    game_platforms = models.db.db.session.execute("SELECT p.platform_name, gap.game_id, p.platform_ico "
-                                               "FROM Games g INNER JOIN Games_and_Platforms gap on g.game_id = gap.game_id "
-                                               "INNER JOIN Platforms p on gap.platform_id = p.platform_id "
-                                               "WHERE gap.game_id = :game_id", {'game_id': game_id})
-    platforms_ico = [base64.b64encode(elem[2]).decode("utf-8") for elem in game_platforms]
+    # НА ПЕРСПЕКТИВУ ЗРОБИТИ ПЛАТФОРМИ ТА ЇХ ВІДОБРАЖЕННЯ ІКОНОК
+    # platforms_ico = models.db.Games.query.join(models.db.games_and_platforms).join(models.db.Platforms).filter((models.db.games_and_platforms.c.game_id == models.db.Games.game_id) & (models.db.games_and_platforms.c.platform_id == models.db.Platforms.platform_id)).all()
+    # platforms_ico = [base64.b64encode(elem.platform_ico).decode("utf-8") for elem in game_details.platforms]
     game_image = base64.b64encode(game_photo.game_photo).decode("utf-8")
-
-    # result_json = {'gameImage': f'{game_image}', 'gameName', 'gamePrice', 'gameGenres', 'gamePlatforms', 'gameDescription'}
 
     if request.method == "POST":
         # file = request.files['file'].read()
@@ -38,43 +28,23 @@ def display_game(game_id: int):
         return render_template("main-page.html")
     return render_template("game.html",
                            game_details=game_details,
-                           game_genres=game_genres,
-                           game_platforms=game_platforms,
-                           platforms_ico=platforms_ico,
-                           game_image=game_image
+                           game_image=game_image,
                            )
 
 @app.route('/games')
 def display_all_games():
-    all_games = models.db.Games.query.all()
-    print(all_games)
-    game_photos = models.db.GameImages.query.all()
-    game_genres = models.db.db.session.execute("SELECT g.game_name, gatp.game_type_id, gt.game_type_name, gatp.game_id "
-                                               "FROM Games g INNER JOIN Games_and_Game_Types gatp on g.game_id = gatp.game_id "
-                                               "INNER JOIN Game_Types gt on gatp.game_type_id = gt.game_type_id ")
-    encoded_game_photos = [base64.b64encode(elem.game_photo).decode("utf-8") for elem in game_photos]
-    print(len(encoded_game_photos))
-    # game_data = models.db.db.session.execute("SELECT g.game_name, g.price, gt.game_type_name, gatp.game_id "
-    #                                          "FROM Games g INNER JOIN Games_and_Game_Types gatp on g.game_id = gatp.game_id "
-    #                                          "INNER JOIN Game_Types gt on gatp.game_type_id = gt.game_type_id "
-    #                                          "INNER JOIN Game_Images gi on g.game_id = gi.game_id")
-
-    return render_template('all_games.html', all_games=all_games, game_genres=game_genres, game_photos=encoded_game_photos)
-   # game_photos=encoded_game_photos,
-   # game_genres=game_genres)
-   # game_data=game_data)
-
-
-def init_db():
-    """ this function create database and all tables in it """
-    models.db.db.app = app
-    models.db.db.create_all()
+    all_games = models.db.Games.query.order_by(models.db.Games.game_id).all()
+    game_images = models.db.GameImages.query.order_by(models.db.GameImages.game_id).all()
+    encoded_game_photos = [base64.b64encode(elem.game_photo).decode("utf-8") for elem in game_images]
+    return render_template('all_games.html',
+                           all_games=all_games,
+                           game_images=encoded_game_photos)
 
 
 if __name__ == '__main__':
     app.jinja_env.filters['zip'] = zip
     app.config['SECRET_KEY'] = 'random-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:BOGDAN12312r@localhost:5432/game_store2"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     models.db.db.init_app(app)
-    init_db()
     app.run(debug=True, use_reloader=True)
--- models/db.py
+++ models/db.py
@@ -3,47 +3,49 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+games_and_game_genres = db.Table('games_and_game_genres',
+    db.Column('game_id', db.Integer, db.ForeignKey('games.game_id')),
+    db.Column('game_type_id', db.Integer, db.ForeignKey('game_genres.game_type_id'))
+)
 
-# game_and_game_types = db.Table('Games_and_Game_Types',
-#     db.Column('game_id', db.Integer, db.ForeignKey('games.game_id')),
-#     db.Column('game_type_id', db.Integer, db.ForeignKey('game_types.game_type_id'))
-# )
 
-
-# games_and_platforms = db.Table('Games_and_Platforms',
-#     db.Column('game_id', db.Integer, db.ForeignKey('games.game_id')),
-#     db.Column('platform_id', db.Integer, db.ForeignKey('platforms.platform_id'))
-# )
+games_and_platforms = db.Table('games_and_platforms',
+    db.Column('game_id', db.Integer, db.ForeignKey('games.game_id')),
+    db.Column('platform_id', db.Integer, db.ForeignKey('platforms.platform_id'))
+)
 
 
 class Games(db.Model):
-    game_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    game_name = db.Column(db.String, nullable=False)
+    __tablename__ = 'games'
+    game_id = db.Column(db.Integer, primary_key=True)
+    game_name = db.Column(db.String(255), nullable=False, unique=True)
     game_description = db.Column(db.Text, nullable=False)
     release_date = db.Column(db.DateTime, nullable=False)
-    number_of_players = db.Column(db.Integer)
+    number_of_players = db.Column(db.Integer, default=0)
     quantity_available = db.Column(db.Integer)
     price = db.Column(db.Numeric(5, 2))
-    # genres = db.relationship('GameTypes',
-    #                          secondary=game_and_game_types,
-    #                          backref=db.backref('game_genres', lazy='joined'))
-    # platforms = db.relationship('Platforms',
-    #                          secondary=games_and_platforms,
-    #                          backref=db.backref('game_platforms', lazy='joined'))
+    genres = db.relationship("GameGenres", secondary=games_and_game_genres,
+                             lazy='subquery', backref=db.backref('classes', lazy=True))
+    platforms = db.relationship("Platforms", secondary=games_and_platforms,
+                             lazy='subquery', backref=db.backref('classes', lazy=True))
 
 
 class GameImages(db.Model):
-    game_id = db.Column(db.Integer)
-    game_image_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    game_photo = db.Column(db.BLOB, nullable=False)
+    __tablename__ = 'game_images'
+    game_id = db.Column(db.Integer, db.ForeignKey('games.game_id'))
+    game_image_id = db.Column(db.Integer, primary_key=True)
+    game_photo = db.Column(db.BLOB, nullable=False) #НЕМА В ПОСТГРЕСІ ТОМУ СТВОРИВ ЧЕРЕЗ КОНСОЛЬ З ТИПОМ BYTEA
+    game = db.relationship("Games", backref=db.backref("games", uselist=False))
 
 
 class Platforms(db.Model):
-    platform_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    platform_name = db.Column(db.VARCHAR(255), nullable=False)
+    __tablename__ = 'platforms'
+    platform_id = db.Column(db.Integer, primary_key=True)
+    platform_name = db.Column(db.String(255), nullable=False)
+    platform_ico = game_photo = db.Column(db.BLOB, nullable=False) #НЕМА В ПОСТГРЕСІ ТОМУ СТВОРИВ ЧЕРЕЗ КОНСОЛЬ З ТИПОМ BYTEA
 
 
-class GameTypes(db.Model):
-    game_type_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    game_type_name = db.Column(db.VARCHAR(255), nullable=False)
-
+class GameGenres(db.Model):
+    __tablename__ = 'game_genres'
+    game_type_id = db.Column(db.Integer, primary_key=True)
+    game_type_name = db.Column(db.String(255), nullable=False)

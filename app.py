32201870826904172a47d9713d9a656e6867ab16
bohdanrom <karpovucc@ukr.net>
2021-04-26 00:43:44 +0300

    Add search

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import base64
 import jinja2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import models.db
 
 app = Flask(__name__)
 env = jinja2.Environment()
 
 
-@app.route('/')
-def main_page():
-    return render_template('main-page.html')
+@app.route('/add-game', methods=["GET", "POST"])
+def add_new_game():
+    return render_template('add_game.html')
 
 
 @app.route('/<int:game_id>', methods=["GET", "POST"])
@@ -31,14 +31,31 @@ def display_game(game_id: int):
                            game_image=game_image,
                            )
 
-@app.route('/games')
+
+@app.route('/')
 def display_all_games():
     all_games = models.db.Games.query.order_by(models.db.Games.game_id).all()
-    game_images = models.db.GameImages.query.order_by(models.db.GameImages.game_id).all()
-    encoded_game_photos = [base64.b64encode(elem.game_photo).decode("utf-8") for elem in game_images]
-    return render_template('all_games.html',
-                           all_games=all_games,
-                           game_images=encoded_game_photos)
+    raw_game_images = models.db.GameImages.query.order_by(models.db.GameImages.game_id).all()
+    game_images = [base64.b64encode(elem.game_photo).decode("utf-8") for elem in raw_game_images]
+    return render_template('all_games.html', all_games=all_games, game_images=game_images)
+
+
+@app.route('/search', methods=["POST"])
+def search():
+    game_name = request.form['game_name']
+    search = f"%{str(game_name).capitalize()}%"
+    raw_all_games = models.db.Games.query.filter(models.db.Games.game_name.like(search)).all()
+    game_ids, all_games = [], []
+    for game in raw_all_games:
+        game_ids.append(game.game_id)
+        all_games.append(game.as_dict())
+    print(all_games)
+    raw_game_images = models.db.GameImages.query.order_by(models.db.GameImages.game_id in game_ids).all()
+    for index, element in enumerate(all_games):
+        element['game_image'] = base64.b64encode(raw_game_images[index].game_photo).decode("utf-8")
+    if all_games:
+        return jsonify(all_games)
+    return jsonify(error=404)
 
 
 if __name__ == '__main__':
@@ -47,4 +64,6 @@ if __name__ == '__main__':
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:BOGDAN12312r@localhost:5432/game_store2"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     models.db.db.init_app(app)
+    models.db.ma = app
+    models.db.db.create_all(app=app)
     app.run(debug=True, use_reloader=True)
--- models/db.py
+++ models/db.py
@@ -1,26 +1,35 @@
+from datetime import date
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 
-games_and_game_genres = db.Table('games_and_game_genres',
+
+games_and_game_genres = db.Table(
+    'games_and_game_genres',
     db.Column('game_id', db.Integer, db.ForeignKey('games.game_id')),
     db.Column('game_type_id', db.Integer, db.ForeignKey('game_genres.game_type_id'))
 )
 
 
-games_and_platforms = db.Table('games_and_platforms',
+games_and_platforms = db.Table(
+    'games_and_platforms',
     db.Column('game_id', db.Integer, db.ForeignKey('games.game_id')),
     db.Column('platform_id', db.Integer, db.ForeignKey('platforms.platform_id'))
 )
 
 
-class Games(db.Model):
+class Mixin():
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Games(db.Model, Mixin):
     __tablename__ = 'games'
     game_id = db.Column(db.Integer, primary_key=True)
     game_name = db.Column(db.String(255), nullable=False, unique=True)
     game_description = db.Column(db.Text, nullable=False)
-    release_date = db.Column(db.DateTime, nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
     number_of_players = db.Column(db.Integer, default=0)
     quantity_available = db.Column(db.Integer)
     price = db.Column(db.Numeric(5, 2))
@@ -30,22 +39,31 @@ class Games(db.Model):
                              lazy='subquery', backref=db.backref('classes', lazy=True))
 
 
-class GameImages(db.Model):
+
+class GameImages(db.Model, Mixin):
     __tablename__ = 'game_images'
     game_id = db.Column(db.Integer, db.ForeignKey('games.game_id'))
     game_image_id = db.Column(db.Integer, primary_key=True)
-    game_photo = db.Column(db.BLOB, nullable=False) #НЕМА В ПОСТГРЕСІ ТОМУ СТВОРИВ ЧЕРЕЗ КОНСОЛЬ З ТИПОМ BYTEA
+    game_photo = db.Column(db.LargeBinary, nullable=False)
     game = db.relationship("Games", backref=db.backref("games", uselist=False))
 
 
-class Platforms(db.Model):
+class Platforms(db.Model, Mixin):
     __tablename__ = 'platforms'
     platform_id = db.Column(db.Integer, primary_key=True)
     platform_name = db.Column(db.String(255), nullable=False)
-    platform_ico = game_photo = db.Column(db.BLOB, nullable=False) #НЕМА В ПОСТГРЕСІ ТОМУ СТВОРИВ ЧЕРЕЗ КОНСОЛЬ З ТИПОМ BYTEA
+    platform_ico = game_photo = db.Column(db.LargeBinary, nullable=False)
 
 
-class GameGenres(db.Model):
+class GameGenres(db.Model, Mixin):
     __tablename__ = 'game_genres'
     game_type_id = db.Column(db.Integer, primary_key=True)
     game_type_name = db.Column(db.String(255), nullable=False)
+
+
+class Customers(db.Model, Mixin):
+    __tablename__ = 'customers'
+    customer_id = db.Column(db.Integer, primary_key=True)
+    customer_photo = db.Column(db.LargeBinary)
+    customer_first_name = db.Column(db.String(255))
+    customer_last_name = db.Column(db.String(255))
